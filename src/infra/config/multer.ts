98a4ea73f626e9cd5aae84af66a27d4903a30706
Ankita <scsit.dev4@sparecare.in>
2025-08-27 13:20:32 +0530

    Cleanup of package.json

--- src/infra/config/multer.ts
+++ src/infra/config/multer.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 // @ts-ignore
-import multerS3 from "multer-s3-v3";
+import multerS3 from "multer-s3";
 import { Request } from "express";
 
 const MAX_SIZE = 150 * 1024 * 1024;
