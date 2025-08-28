36ca2c26044aab1acf131f5f5ab0736a4c49803b
Ankita <scsit.dev4@sparecare.in>
2025-08-28 15:09:14 +0530

    cors allow

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import routes from "../interface/http/routes/index"; 
+import cors from "cors"; 
 import { poolPromise } from "../infra/db/mssql/connection";
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use("/api", routes);
 
 (async () => {
